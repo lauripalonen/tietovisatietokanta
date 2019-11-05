73a0588516993048199e68e11dbd0a4887275f8b
lauripalonen <palonen.lauri@gmail.com>
2019-11-05 18:41:35 +0200

    init quiz directory

---
+++ application/quizzes/models.py
@@ -0,0 +1,21 @@
+from application import db
+
+class Quiz(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+  date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+  onupdate = db.func.current_timestamp())
+
+  question = db.Column(db.String(144), nullable=False)
+  answer = db.Column(db.String(144), nullable=False)
+  category = db.Column(db.String(144))
+  answeredCorrectly = db.Column(db.Boolean)
+  quizDate = db.Column(db.DateTime)
+
+  def __init__(self, question, answer):
+      self.question = question
+      self.answer = answer
+      self.answeredCorrectly = False
+
+
+  
