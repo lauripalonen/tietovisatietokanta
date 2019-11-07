41e85f4c66f2304d25296feb9e4bb141907cf162
lauripalonen <palonen.lauri@gmail.com>
2019-11-07 14:22:29 +0200

    update imports

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quizlog.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
@@ -11,5 +10,6 @@ db = SQLAlchemy(app)
 from application import views
 
 from application.questions import models
+from application.questions import views
 
 db.create_all()
