da88de8c0aa883a542be1ef42fbb067091c27590
lauripalonen <palonen.lauri@gmail.com>
2019-11-07 14:24:03 +0200

    update db schema

--- application/questions/models.py
+++ application/questions/models.py
@@ -4,7 +4,7 @@ class Question(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
   date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-  onupdate = db.func.current_timestamp())
+  onupdate=db.func.current_timestamp())
 
   question = db.Column(db.String(144), nullable=False)
   answer = db.Column(db.String(144), nullable=False)
@@ -12,10 +12,10 @@ class Question(db.Model):
   answeredCorrectly = db.Column(db.Boolean)
   quizDate = db.Column(db.DateTime)
 
-  def __init__(self, question, answer):
+  def __init__(self, question, answer, answeredCorrectly):
       self.question = question
       self.answer = answer
-      self.answeredCorrectly = False
+      self.answeredCorrectly = answeredCorrectly
 
 
   
