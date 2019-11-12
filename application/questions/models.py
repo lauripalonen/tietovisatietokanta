cbd6e6e92d845dc44209cca6eb8a8cf62d4299ff
lauripalonen <palonen.lauri@gmail.com>
2019-11-12 20:30:52 +0200

    add authentication functionalities

--- application/questions/models.py
+++ application/questions/models.py
@@ -1,21 +1,19 @@
 from application import db
 
+
 class Question(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-  date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-  onupdate=db.func.current_timestamp())
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
 
-  question = db.Column(db.String(144), nullable=False)
-  answer = db.Column(db.String(144), nullable=False)
-  category = db.Column(db.String(144))
-  answeredCorrectly = db.Column(db.Boolean)
-  quizDate = db.Column(db.DateTime)
+    question = db.Column(db.String(144), nullable=False)
+    answer = db.Column(db.String(144), nullable=False)
+    category = db.Column(db.String(144))
+    answeredCorrectly = db.Column(db.Boolean)
+    quizDate = db.Column(db.DateTime)
 
-  def __init__(self, question, answer, answeredCorrectly):
-      self.question = question
-      self.answer = answer
-      self.answeredCorrectly = answeredCorrectly
-
-
-  
+    def __init__(self, question, answer, answeredCorrectly):
+        self.question = question
+        self.answer = answer
+        self.answeredCorrectly = answeredCorrectly
