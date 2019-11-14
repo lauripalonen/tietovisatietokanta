175ebea8840ebea4a25578f97430f4186b308d97
lauripalonen <palonen.lauri@gmail.com>
2019-11-14 11:19:49 +0200

    add validators

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,16 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
-  
+from wtforms import PasswordField, StringField, validators
+
+errMsgU = "Username must be atleast 1 character long"
+errMsgPw = "Password must be 5 characters long"
+
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
-  
+
     class Meta:
         csrf = False
 
-class SignupForm(FlaskForm):
-  username = StringField("Username")
-  password = PasswordField("Password")
 
-  class Meta:
-    csrf = False
+class SignupForm(FlaskForm):
+    username = StringField("Username", [validators.Length(min=1, message=errMsgU)])
+    password = PasswordField("Password", [validators.Length(min=5, message=errMsgPw)])
+
+    class Meta:
+        csrf = False
