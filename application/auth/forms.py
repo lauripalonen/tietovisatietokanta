8fe1ae2a1448c2c8292933bfb456719036233e6a
lauripalonen <palonen.lauri@gmail.com>
2019-11-14 11:53:33 +0200

    change validation requirements

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
 
-errMsgU = "Username must be atleast 1 character long"
+errMsgU = "Username must be atleast 2 characters long"
 errMsgPw = "Password must be 5 characters long"
 
 class LoginForm(FlaskForm):
@@ -13,7 +13,7 @@ class LoginForm(FlaskForm):
 
 
 class SignupForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=1, message=errMsgU)])
+    username = StringField("Username", [validators.Length(min=2, message=errMsgU)])
     password = PasswordField("Password", [validators.Length(min=5, message=errMsgPw)])
 
     class Meta:
