8e898ade3682ce69c2c09317e852580ff755bff0
lauripalonen <palonen.lauri@gmail.com>
2019-11-21 22:53:54 +0200

    add team form

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,6 +3,7 @@ from wtforms import PasswordField, StringField, validators
 
 errMsgU = "Username must be atleast 2 characters long"
 errMsgPw = "Password must be 5 characters long"
+errMsgT = "Team name must be atleast 2 characters long"
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -15,6 +16,7 @@ class LoginForm(FlaskForm):
 class SignupForm(FlaskForm):
     username = StringField("Username", [validators.Length(min=2, message=errMsgU)])
     password = PasswordField("Password", [validators.Length(min=5, message=errMsgPw)])
+    team =StringField("Team", [validators.Length(min=2, message=errMsgT)])
 
     class Meta:
         csrf = False
