5698ed0edf2a1d3fd7464ed6e9a6c70c5b3d0f50
lauripalonen <palonen.lauri@gmail.com>
2019-11-21 22:58:48 +0200

    edit field label

--- application/teams/forms.py
+++ application/teams/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class TeamForm(FlaskForm):
-    team_name = StringField("Team", [validators.Length(min=1, message="Team name must be atleast 1 character long")])
+    team_name = StringField("Create new team", [validators.Length(min=1, message="Team name must be atleast 1 character long")])
 
     class Meta:
         csrf = False
