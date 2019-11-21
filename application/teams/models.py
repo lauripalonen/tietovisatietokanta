15f8e165b73e7eb40f89715c40814b32507359b4
lauripalonen <palonen.lauri@gmail.com>
2019-11-21 15:08:17 +0200

    import user_team helper table

--- application/teams/models.py
+++ application/teams/models.py
@@ -1,4 +1,6 @@
 from application import db
+from application import user_team
+from application.auth.models import User
 
 class Team(db.Model):
 
@@ -10,7 +12,7 @@ class Team(db.Model):
     name = db.Column(db.String(144), nullable=False)
 
     questions = db.relationship("Question", backref="team", lazy=True)
-    users = db.relationship("User", backref="team", lazy=True)
+    users = db.relationship("User", secondary="user_team", backref=db.backref('teamos', lazy=True))
 
     def __init__(self, name):
         self.name = name
