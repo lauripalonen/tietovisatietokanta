296864a86fff4e20c2469d3dda1d454eb68e8395
lauripalonen <palonen.lauri@gmail.com>
2019-11-21 22:55:50 +0200

    include team to sign up form

--- application/auth/views.py
+++ application/auth/views.py
@@ -4,16 +4,18 @@ from flask_login import login_user, logout_user
 from application import app, bcrypt, db
 from application.auth.models import User
 from application.auth.forms import LoginForm, SignupForm
+from application.teams.models import Team
 
-@app.route("/auth/login", methods = ["GET", "POST"])
+
+@app.route("/auth/login", methods=["GET", "POST"])
 def auth_login():
     if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+        return render_template("auth/loginform.html", form=LoginForm())
 
     form = LoginForm(request.form)
 
     user = User.query.filter_by(username=form.username.data).first()
-    
+
     if not user:
         return render_template("auth/loginform.html", form=form,
                                error="No such username")
@@ -25,31 +27,47 @@ def auth_login():
     login_user(user)
     return redirect(url_for("index"))
 
+
 @app.route("/auth/logout")
 def auth_logout():
-  logout_user()
-  return redirect(url_for("index"))
+    logout_user()
+    return redirect(url_for("index"))
 
-@app.route("/auth/signup", methods = ["GET", "POST"])
+
+@app.route("/auth/signup", methods=["GET", "POST"])
 def auth_signup():
-  if request.method == "GET":
-      return render_template("auth/signupform.html", form = SignupForm())
+    if request.method == "GET":
+        return render_template("auth/signupform.html", form=SignupForm())
 
-  form = SignupForm(request.form)
-  if not form.validate():
-    return render_template("auth/signupform.html", form = form, error="Username or password too short")
+    form = SignupForm(request.form)
+    if not form.validate():
+        return render_template("auth/signupform.html", form=form, error="Username or password too short")
 
-  username = form.username.data
-  password = bcrypt.generate_password_hash(form.password.data)
-  user = User.query.filter_by(username=username).first()
+    username = form.username.data
+    password = bcrypt.generate_password_hash(form.password.data)
+    team_name = form.team.data
+    user = User.query.filter_by(username=username).first()
 
-  if user:
-    return render_template("auth/signupform.html", form=form, error="Username already exists")
+    if user:
+        return render_template("auth/signupform.html", form=form, error="Username already exists")
 
-  new_user = User(username=username, password=password)
+    new_user = User(username=username, password=password)
+    db.session().add(new_user)
 
-  db.session().add(new_user)
-  db.session().commit()
+    team = Team.query.filter_by(name=team_name).first()
 
-  login_user(new_user)
-  return redirect(url_for("index"))
+    if not team:
+        team = Team(team_name)
+        db.session().add(team)
+
+    db.session().commit()
+
+    created_user = User.query.filter_by(username=username).first()
+
+    new_user.team_id = team.id
+    team.members.append(created_user)
+
+    db.session().commit()
+
+    login_user(new_user)
+    return redirect(url_for("index"))
