9bea331a8f7d100a2996f45071adcd46f0cc9ab3
lauripalonen <palonen.lauri@gmail.com>
2019-11-22 12:12:42 +0200

    disable heroku logging

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from application import app
 
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+#app.logger.addHandler(logging.StreamHandler(sys.stdout))
+#app.logger.setLevel(logging.ERROR)
 
 
 if __name__ == '__main__':
