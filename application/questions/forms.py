b3c4e7db6444bf6c471dbc1b2c3c062eccb209ec
lauripalonen <palonen.lauri@gmail.com>
2019-11-27 15:26:13 +0200

    add max length validator for question, answer and category

--- application/questions/forms.py
+++ application/questions/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, validators
 
-errMsgQ = "Question must be atleast 2 characters long"
+errMsgQ = "Question must be between 2 to 144 characters long"
 errMsgA = "Answer field cannot be empty"
-errMsgC = "Category must be atleast 3 characters long"
+errMsgC = "Category must between 3 to 50 characters long"
 
 class QuestionForm(FlaskForm):
-    question = StringField("Question", [validators.Length(min=2, message=errMsgQ)])
-    answer = StringField("Answer", [validators.Length(min=1, message=errMsgA)])
-    category = StringField("Category", {validators.Length(min=3, message=errMsgC)})
+    question = StringField("Question", [validators.Length(min=2, max=144, message=errMsgQ)])
+    answer = StringField("Answer", [validators.Length(min=1, max=144, message=errMsgA)])
+    category = StringField("Category", {validators.Length(min=3, max=50, message=errMsgC)})
     correct = BooleanField("Answered correctly")
-     
+
     class Meta:
         csrf = False
 
