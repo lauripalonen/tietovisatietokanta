d3eb40ed300bcecdaecf39e4bd143672548fed34
lauripalonen <palonen.lauri@gmail.com>
2019-12-13 18:29:28 +0200

    add method to clear user_team table

--- application/user_team.py
+++ application/user_team.py
@@ -1,5 +1,17 @@
 from application import db
+from sqlalchemy.sql import text
+
 
 user_team = db.Table('user_team', 
     db.Column('user_id', db.Integer, db.ForeignKey('account.id')),
     db.Column('team_id', db.Integer, db.ForeignKey('team.id')))
+
+def clear_table():
+        print("REACHED user_team DELETATION PROCESS")
+
+        stmt = text("DELETE FROM user_team WHERE user_team.user_id !="
+                   " (SELECT id FROM account WHERE username = 'ADMIN')")
+
+        db.engine.execute(stmt)
+
+        return print("user_team table cleared")
